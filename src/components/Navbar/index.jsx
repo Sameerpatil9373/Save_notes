5a0765200dba00f21b1793e32de9de5984a6a613
Sameerpatil9373 <sameerpatil9373@gmail.com>
2025-09-20 18:16:33 +0530

    chnage in the logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
     return(
         <header className='flex px-4 py-2 gap-2 border-b-2 border-gray-400'>
             <div className='w-12 h-12'>
-                <img className='h-full w-full' src={logo}/>
+                <img className='h-full w-full' src={logo} alt='Logo'/>
             </div>
             <h1 className='text-indigo-800 text-4xl font-bold'>NoteIt</h1>
             </header>
